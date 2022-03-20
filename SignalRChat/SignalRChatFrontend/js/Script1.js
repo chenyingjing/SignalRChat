2357309f965156dd15785f5be433b2693329c539
chenyingjing <dcba63@gmail.com>
2022-03-20 12:01:50 +0800

    Send post request by jquery.

---
+++ SignalRChat/SignalRChatFrontend/js/Script1.js
@@ -0,0 +1,31 @@
+$(function () {
+    $("#postButton").click(function () {
+        //alert("hello");
+        //$.post("https://localhost:5001/api/File", {  }, function (data) {
+        //    // actions upon receiving the reply
+        //    alert("Data received: " + data);
+        //});
+
+        $.ajax({
+            type: 'POST',
+            url: 'https://localhost:5001/api/File',
+            //data: "{'name': 'nouman','email': 'noumandilawar@gmail.com','mobile_number': 03324412764,'employee_number': 4556, 'gender': 1,'password': '1234567891234567','language': 1}",
+            //data: "sbc",
+            //data: '{"id": "nouman","name":"nouman"}',
+            data: '{"field1": "string"}',
+            contentType: "text/json",
+            dataType: 'text',
+            crossDomain: true,
+            success: OnGetMemberSuccess,
+            error: OnGetMemberError
+        });
+    });
+
+    function OnGetMemberSuccess(data, status) {
+        alert(data + "   " + status);
+    }
+
+    function OnGetMemberError(request, status, error) {
+        alert(request + "  " + error + "   " + status);
+    }
+})
---
+++ SignalRChat/SignalRChatWebAPI/Controllers/FileController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SignalRChatWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileController : ControllerBase
+    {
+        //// GET: api/<FileController>
+        //[HttpGet]
+        //public IEnumerable<string> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
+
+        //// GET api/<FileController>/5
+        //[HttpGet("{id}")]
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        // POST api/<FileController>
+        [HttpPost]
+        public void Post([FromBody] MyData value)
+        //public void Post([FromBody] string value)
+        {
+        }
+
+
+    }
+}
