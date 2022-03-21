01b1c3fea7028db4ab346543d7a4039b9df2ec12
chenyingjing <dcba63@gmail.com>
2022-03-21 10:45:21 +0800

    Send and receive message by SignalR.

--- SignalRChat/SignalRChatClient/MainWindow.xaml.cs
+++ SignalRChat/SignalRChatClient/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,57 @@ namespace SignalRChatClient
     /// </summary>
     public partial class MainWindow : Window
     {
+        HubConnection connection;
         public MainWindow()
         {
             InitializeComponent();
+
+            connection = new HubConnectionBuilder()
+                .WithUrl("https://localhost:5001/ChatHub")
+                .Build();
+
+            connection.Closed += async (error) =>
+            {
+                await Task.Delay(new Random().Next(0, 5) * 1000);
+                await connection.StartAsync();
+            };
+        }
+
+        private async void connectButton_Click(object sender, RoutedEventArgs e)
+        {
+            connection.On<string, string>("ReceiveMessage", (user, message) =>
+            {
+                this.Dispatcher.Invoke(() =>
+                {
+                    var newMessage = $"{user}: {message}";
+                    messagesList.Items.Add(newMessage);
+                });
+            });
+
+            try
+            {
+                await connection.StartAsync();
+                messagesList.Items.Add("Connection started");
+                connectButton.IsEnabled = false;
+                sendButton.IsEnabled = true;
+            }
+            catch (Exception ex)
+            {
+                messagesList.Items.Add(ex.Message);
+            }
+        }
+
+        private async void sendButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                await connection.InvokeAsync("SendMessage",
+                    userTextBox.Text, messageTextBox.Text);
+            }
+            catch (Exception ex)
+            {
+                messagesList.Items.Add(ex.Message);
+            }
         }
     }
 }
